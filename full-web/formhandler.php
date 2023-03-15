0dd44bb4783f3f9f9842a3bb36115150fdecc4c5
imranmatano6@gmail.com <you@example.com>
2023-03-15 23:04:56 +0300

    Added the PHP file for contact

---
+++ full-web/formhandler.php
@@ -0,0 +1,27 @@
+<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'victormunyi006@gmail.com';
+
+$email_subject = 'New form submission';
+
+$email_body = "User name: $name.\n".
+              "User email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User message: $message.\n";
+
+$to = 'munyivictor6@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+?>
